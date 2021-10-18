c63fd791920af47f07af1a95579ed4b56b066e0a
iasebsil83 <i.a.sebsil83@gmail.com>
2021-10-18 17:28:42 +0200

    set basic hadamar circuit

--- src/grover.py
+++ src/grover.py
@@ -2,5 +2,32 @@
 
 
 
-# ---------------- DECLARATIONS ----------------
-print("Grover")
+# ---------------- IMPORTATION ----------------
+
+#quantum lib
+import qiskit as q
+
+
+
+
+
+
+# ---------------- GROVER ----------------
+
+#circuit
+def getGroverCircuit(qbit_nbr):
+	circuit = q.QuantumCircuit(qbit_nbr+1, qbit_nbr)
+
+	#for each qbit
+	for n in range(qbit_nbr):
+
+		#set Hadamar gates
+		circuit.h(n)
+
+		#set grover gates
+		#circuit.grover(n, last)
+
+		#set measure
+		circuit.measure(n,n)
+
+	return circuit
--- src/qomputer.py
+++ src/qomputer.py
@@ -16,21 +16,27 @@ import qiskit as q
 class Qomputer:
 
 	#initialization
-	def __init__(self, qbit_nbr, measure_nbr, shots):
+	def __init__(self, qbit_nbr, measure_nbr):
 		self.simulator = q.Aer.get_backend('qasm_simulator')
 		self.circuit   = q.QuantumCircuit(qbit_nbr, measure_nbr)
-		self.shots     = shots
 
 	#console display
 	def showCircuit(self):
-		print( circuit.draw(output='text') )
+		print( self.circuit.draw(output='text') )
 
 	#execution
-	def getResults(self):
-		return q.execute(
+	def run(self, shots):
+		raw_results = q.execute(
 			self.circuit,
 			self.simulator,
-			shots=self.shots
+			shots=shots
 		).result().get_counts(
 			self.circuit
 		)
+
+		#format results
+		results = []
+		for r in raw_results:
+			results.append( raw_results[r] )
+
+		return results
